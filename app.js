bd62513bca866cb4b196d1a64e222d855aa8f209
HoàngThanh Giang <hoangthanhgiang22@gmail.com>
2024-11-03 02:41:56 +0700

    ok

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ const app = angular.module("myApp", ["ngRoute"]);
         .when("/quanlysanpham", {
             templateUrl: "./Views/quanlysanpham.html"
         })
+        .when("/addsanpham", {
+            templateUrl: "./Views/addsanpham.html"
+        })
+        .when("/login", {
+            templateUrl: "./Views/login.html"
+        })
+        .when("/loginotp", {
+            templateUrl: "./Views/loginotp.html"
+        })
+        .when("/quenmatkhau", {
+            templateUrl: "./Views/quenmatkhau.html"
+        })
         .otherwise({
             redirectTo: "/"
         });
